c9868534e039d341d31799a772881f37117d26d5
Eternal-Allies <eternalallies.au@gmail.com>
2026-01-15 12:37:58 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
@@ -78,7 +78,7 @@ const server = http.createServer(app);
 // 🔥 Attach Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   },
 });
